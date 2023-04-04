0a63839bcf753f0db6cf773ad034d825747b6916
Matthew Groover <mgroov@nmsu.edu>
2023-04-04 13:47:24 -0600

    comments on decrypt

--- ransom/r3decrypt.py
+++ ransom/r3decrypt.py
@@ -3,6 +3,7 @@
 key=['R','3','V','3','R','5','3']
 
 #loop through till then end of each file
+#code for loop inspired by: https://stackoverflow.com/questions/6787233/python-how-to-read-bytes-from-file-and-save-it
 with open("secret.txt.pay_up", "rb") as in_file,open("secret.txt", "wb") as out_file:
     while True:
         chunk = bytearray(in_file.read(7)) #create a bytearray of size seven
@@ -14,4 +15,4 @@ with open("secret.txt.pay_up", "rb") as in_file,open("secret.txt", "wb") as out_
             #print(i)
             chunk[i]=chunk[i]^ord(key[i]) #xor the piece
             
-        out_file.write(chunk)
+        out_file.write(chunk) #write the decrypted bytes
